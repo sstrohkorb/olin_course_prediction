e636f17b215836379cf8ce8374c1f8d360c3266a
Sarah Strohkorb <sarah.strohkorb@gmail.com>
2014-02-11 16:08:30 -0500

    Made it so that we can see the top 6 correlated classes

--- big_3_majors_linear_regression.py
+++ big_3_majors_linear_regression.py
@@ -25,8 +25,7 @@ def create_course_enrollment_data(students, courses, professors, desired_course,
 
   course_list = []
   for course in courses:
-    course_list.append(courses[course].course_number)
-    course_list.sort()
+    course_list.append(course[0])
 
   course_dict = {course_list[i]: i for i in range(len(course_list))}
 
@@ -69,21 +68,45 @@ def test_logistic(logistic, x_test, y_test):
   # print "%f correct" %percent_correct
   return percent_correct
 
+def determine_highest_weighted_courses(logistic, all_courses_list, number_of_courses):
+  # find the classes with highest absolute value coefficients
+  # determine whether they're positive or negative
+  sorted_highly_weighted_courses = sorted(zip(logistic.coef_[0], all_courses_list), key=lambda x:abs(x[0]), reverse=True)
+  return sorted_highly_weighted_courses[:number_of_courses]
+
+def prediction_strength_for_a_course(course_number, course_name, course_semester, number_of_iterations, all_courses_list):
+  [x_vector, y_vector] = create_course_enrollment_data(students, all_courses_list, professors, course, semester)
+  test_results = []
+  for i in range(number_of_iterations):
+    [logistic, x_test, y_test] = make_logistic(x_vector, y_vector, 20)
+    test_results.append(test_logistic(logistic, x_test, y_test))
+  fat_courses = determine_highest_weighted_courses(logistic, all_courses_list, 6)
+  temp_str = ""
+  for correlation, course_info in fat_courses:
+    if correlation < 0:
+      temp_str += "-"
+    else: 
+      temp_str += "+"
+    temp_str += " " + course_info[1] + '\n'
+  return [sum(test_results)/len(test_results), temp_str]
 
 
 [students, courses, professors] = get_course_data('../anonymizeddata_UpToFall2013.csv')
+all_courses_list = []
+for course in courses: 
+  all_courses_list.append([courses[course].course_number, courses[course].title])
 
-course_list = ['ENGR3420', 'ENGR2250', 'ENGR2510', 'ENGR3320', 'MTH3120', 'SCI2320', 'ENGR3370', 'AHSE1122', 'SCI2199', 'ENGR3380']
-course_names = ['AnalDig', 'UOCD', 'SoftDes', 'MechSolids', 'PDEs', 'OChem', 'Controls', 'Wired', 'Relativity', 'DFM']
+#course_list = ['ENGR3420', 'ENGR2250', 'ENGR2510', 'ENGR3320', 'MTH3120', 'SCI2320', 'ENGR3370', 'SCI2199', 'ENGR3380']
+#course_names = ['AnalDig', 'UOCD', 'SoftDes', 'MechSolids', 'PDEs', 'OChem', 'Controls', 'Relativity', 'DFM']
+#course_semester = ['JR', 'SO', 'SO', 'SO', 'JR', 'SO', 'SR', 'SO', 'SR']
 
-for course, course_name in zip(course_list, course_names):
-  [x_vector, y_vector] = create_course_enrollment_data(students, courses, professors, course, 'JR')
+course_list = ['ENGR3420', 'ENGR3380', 'ENGR2330', 'ENGR3370', 'ENGR3210', 'ENGR3220', 'SCI1210']
+course_names = ['AnalDig', 'DFM', 'MechProto', 'Controls', 'Sustainable Design', 'HFID', 'ModBio']
+course_semester = ['JR', 'SR', 'JR', 'SR', 'JR', 'JR', 'SR']
 
-  test_results = []
-  for i in range(50):
-    [logistic, x_test, y_test] = make_logistic(x_vector, y_vector, 10)
-    test_results.append(test_logistic(logistic, x_test, y_test))
-
-  print '%s: %s - %f' %(course, course_name, sum(test_results)/len(test_results))
+for course, course_name, semester in zip(course_list, course_names, course_semester):
+  [prediction_strength, description] = prediction_strength_for_a_course(course, course_name, semester, 50, all_courses_list)
+  print '%s: %s - %f' %(course_name, semester, prediction_strength)
+  print description
 
   # print logistic.get_params(deep=True)
