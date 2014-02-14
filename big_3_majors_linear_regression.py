921cee6fa94c609eb36950d9492daa41985bf0ac
Sarah Strohkorb <sarah.strohkorb@gmail.com>
2014-02-14 15:01:19 -0500

    calculating the area under the ROC curve, we smart

--- big_3_majors_linear_regression.py
+++ big_3_majors_linear_regression.py
@@ -3,7 +3,7 @@ from parse_course_data import *
 import numpy as np
 from sklearn import datasets
 from sklearn import linear_model
-
+import matplotlib.pyplot as plt
 
 
 def make_training_data(x_vector, y_vector, test_size):
@@ -60,7 +60,7 @@ def make_logistic(all_x_vectors, all_y_values, test_size):
   logistic.fit(x_train, y_train)
   return [logistic, x_test, y_test]
 
-def test_logistic(logistic, x_test, y_test):
+def test_logistic_binary(logistic, x_test, y_test):
   num_correct = 0
   for i in range(len(x_test)):
     predicted = logistic.predict(x_test[i])
@@ -70,6 +70,41 @@ def test_logistic(logistic, x_test, y_test):
   # print "%f correct" %percent_correct
   return percent_correct
 
+def area_under_curve(x_vals, y_vals):
+  total_area = 0
+  for i in range(len(x_vals) - 1):
+    total_area += (x_vals[i+1] - x_vals[i]) * y_vals[i+1]
+  return total_area
+
+def compute_ROC_for_logistic(logistic, x_test, y_test):
+  prob = logistic.predict_proba(x_test)
+  sorted_probabilities = sorted(zip(prob, y_test), key=lambda x:x[0][1], reverse=True)
+  true_pos = []
+  false_pos = []
+  true_pos_total = 0
+  false_pos_total = 0
+  total_pos = sum(y_test)
+  total_neg = len(y_test) - total_pos
+  for (probabilities, y_value) in sorted_probabilities:
+    if y_value == 1:
+      true_pos_total += 1.0
+    else: 
+      false_pos_total += 1.0
+    if total_pos == 0:
+      true_pos.append(0)
+    else: 
+      true_pos.append(true_pos_total/total_pos)
+    if total_neg == 0:
+      true_neg.append(0)
+    else: 
+      false_pos.append(false_pos_total/total_neg)
+
+  # area under the ROC curve
+  area = area_under_curve(false_pos, true_pos)
+  
+  return area
+  
+
 def determine_highest_weighted_courses(logistic, all_courses_list, number_of_courses):
   # find the classes with highest absolute value coefficients
   # determine whether they're positive or negative
@@ -78,19 +113,26 @@ def determine_highest_weighted_courses(logistic, all_courses_list, number_of_cou
 
 def prediction_strength_for_a_course(course_number, course_name, course_semester, number_of_iterations, all_courses_list):
   [x_vector, y_vector] = create_course_enrollment_data(students, all_courses_list, professors, course, semester)
-  test_results = []
+  #test_results = []
+  ROC_results = []
   for i in range(number_of_iterations):
-    [logistic, x_test, y_test] = make_logistic(x_vector, y_vector, 20)
-    test_results.append(test_logistic(logistic, x_test, y_test))
-  fat_courses = determine_highest_weighted_courses(logistic, all_courses_list, 6)
+    [logistic, x_test, y_test] = make_logistic(x_vector, y_vector, 40)
+    #test_results.append(test_logistic_binary(logistic, x_test, y_test))
+    ROC_results.append(compute_ROC_for_logistic(logistic, x_test, y_test))
+
+  print sum(ROC_results)/len(ROC_results)
+  
+  # Determine highly weighted courses
+  highly_weighted_courses = determine_highest_weighted_courses(logistic, all_courses_list, 6)
   temp_str = ""
-  for correlation, course_info in fat_courses:
+  for correlation, course_info in highly_weighted_courses:
     if correlation < 0:
       temp_str += "-"
     else: 
       temp_str += "+"
     temp_str += " " + course_info[1] + '\n'
-  return [sum(test_results)/len(test_results), temp_str]
+  
+  return temp_str
 
 
 [students, courses, professors] = get_course_data('../anonymizeddata_UpToFall2013.csv')
@@ -102,13 +144,11 @@ for course in courses:
 #course_names = ['AnalDig', 'UOCD', 'SoftDes', 'MechSolids', 'PDEs', 'OChem', 'Controls', 'Relativity', 'DFM']
 #course_semester = ['JR', 'SO', 'SO', 'SO', 'JR', 'SO', 'SR', 'SO', 'SR']
 
-course_list = ['ENGR3420', 'ENGR3380', 'ENGR2330', 'ENGR3370', 'ENGR3210', 'ENGR3220', 'SCI1210', 'ENGR3392']
-course_names = ['AnalDig', 'DFM', 'MechProto', 'Controls', 'Sustainable Design', 'HFID', 'ModBio', 'Robo2']
-course_semester = ['JR', 'SR', 'JR', 'SR', 'JR', 'JR', 'SR', 'JR']
+course_list = ['AHSE1100', 'ENGR3420', 'ENGR3380', 'ENGR2330', 'ENGR3370', 'ENGR3210', 'ENGR3220', 'SCI1210', 'ENGR3392', 'ENGR2510']
+course_names = ['HistofTech', 'AnalDig', 'DFM', 'MechProto', 'Controls', 'Sustainable Design', 'HFID', 'ModBio', 'Robo2', 'SoftDes']
+course_semester = ['FF', 'JR', 'SR', 'JR', 'SR', 'JR', 'JR', 'SR', 'JR', 'FR']
 
 for course, course_name, semester in zip(course_list, course_names, course_semester):
-  [prediction_strength, description] = prediction_strength_for_a_course(course, course_name, semester, 50, all_courses_list)
-  print '%s: %s - %f' %(course_name, semester, prediction_strength)
+  description = prediction_strength_for_a_course(course, course_name, semester, 50, all_courses_list)
+  print '%s: %s' %(course_name, semester)
   print description
-
-  # print logistic.get_params(deep=True)
