5603938da1c4454b250a75b58b16a3e8b0512c18
Sarah Strohkorb <sarah.strohkorb@gmail.com>
2014-02-27 00:52:47 -0500

    Added a baseline comparison for all of the course predictions

--- big_3_majors_linear_regression.py
+++ big_3_majors_linear_regression.py
@@ -5,6 +5,8 @@ from sklearn import datasets
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 
+semesters = ['FF', 'FR', 'SO1', 'SO2', 'JR1', 'JR2', 'SR1', 'SR2']
+
 
 def make_training_data(x_vector, y_vector, test_size):
   """ Takes as input all of the x values in a list and the y values in a list and then designates
@@ -19,13 +21,45 @@ def make_training_data(x_vector, y_vector, test_size):
 
   return [x_train, y_train, x_test, y_test]
 
+def freuqency_based_prediction_strength(students, courses, professors, desired_course, desired_semester):
+  """ Determines a baseline prediction strength based on how many students have taken the course 
+      in that semester as to provide a comparison for how good our model actually is
+  """
+  for i in range(len(semesters)):
+    if semesters[i] == desired_semester:
+      acceptable_semesters = semesters[:i]
+      break
+
+  total_not_taken_course_before = 0
+  num_who_took_course_previously = 0
+  num_who_took_course_during_desired_semester = 0
+
+  for student_id in students:
+    for course_offering in students[student_id].list_of_course_offerings:
+      course_no = course_offering.course.course_number
+      if course_no == desired_course:
+        if course_offering.student_year in acceptable_semesters:
+          num_who_took_course_previously += 1
+        if course_offering.student_year == desired_semester:
+          num_who_took_course_during_desired_semester += 1
+
+  total_not_taken_course_before = len(students) - num_who_took_course_previously
+
+  if total_not_taken_course_before == 0:
+    baseline_prediction_strength = 0.5
+  else: 
+    baseline_prediction_strength = float(num_who_took_course_during_desired_semester)/float(total_not_taken_course_before)
+  
+  return baseline_prediction_strength
+
+
+
 def create_course_enrollment_data(students, courses, professors, desired_course, desired_semester):
   """ Setup the x and y vectors that contain all of the data that the model will take in based
       on the enrollment data that is input. 
       all_x_vectors represents the inputs to the linear regression model
       all_y_values represents the outputs to the linear regression model
   """
-  semesters = ['FF', 'FR', 'SO1', 'SO2', 'JR1', 'JR2', 'SR1', 'SR2']
   for i in range(len(semesters)):
     if semesters[i] == desired_semester:
       acceptable_semesters = semesters[:i]
@@ -146,6 +180,7 @@ def prediction_strength_for_a_course(course_number, course_name, course_semester
       determine what the highested weighted courses are for a given course 
   """
   [x_vector, y_vector] = create_course_enrollment_data(students, all_courses_list, professors, course, semester)
+  frequency_baseline = freuqency_based_prediction_strength(students, all_courses_list, professors, course, semester)
   #test_results = []
   ROC_results = []
   for i in range(number_of_iterations):
@@ -153,7 +188,8 @@ def prediction_strength_for_a_course(course_number, course_name, course_semester
     #test_results.append(test_logistic_binary(logistic, x_test, y_test))
     ROC_results.append(compute_ROC_for_logistic(logistic, x_test, y_test))
 
-  print sum(ROC_results)/len(ROC_results)
+  our_algorithm_str =  ("Our algorithm: %.4f" % (sum(ROC_results)/len(ROC_results)))
+  baseline_str =       ("Baseline:      %.4f" % frequency_baseline)
   
   # Determine highly weighted courses
   highly_weighted_courses = determine_highest_weighted_courses(logistic, all_courses_list, 6)
@@ -165,7 +201,7 @@ def prediction_strength_for_a_course(course_number, course_name, course_semester
       temp_str += "+"
     temp_str += " " + course_info[1] + '\n'
   
-  return temp_str
+  return [our_algorithm_str, baseline_str, temp_str]
 
 
 if __name__ == "__main__":
@@ -183,6 +219,8 @@ if __name__ == "__main__":
   course_semester = ['FF', 'JR2', 'SR2', 'JR2', 'SR2', 'JR1', 'JR1', 'SR2', 'JR2', 'FR']
 
   for course, course_name, semester in zip(course_list, course_names, course_semester):
-    description = prediction_strength_for_a_course(course, course_name, semester, 50, all_courses_list)
+    [our_algorithm_str, baseline_str, description] = prediction_strength_for_a_course(course, course_name, semester, 50, all_courses_list)
     print '%s: %s' %(course_name, semester)
+    print our_algorithm_str
+    print baseline_str
     print description
