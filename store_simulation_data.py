c9f90eda0565cc72b748e6ecd07aa640ba462e04
Sarah Strohkorb <sarah.strohkorb@gmail.com>
2014-04-22 11:58:59 -0400

    Preparing output file for the simulation of just prereg data

--- store_simulation_data.py
+++ store_simulation_data.py
@@ -1,13 +1,13 @@
 from xlwt import Workbook
 
-def store_simulation_data(course_list, courses, semester_names, sim_data, destination_file_name="test_data.xls"):
+def store_simulation_data(course_list, courses, semester_names, sim_data, prereg_predicted_data, destination_file_name="test_data.xls"):
   """
   course_list -> the list of courses we're predicting (course numbers)
   """
   
   w = Workbook()
 
-  column_headings = ["Semester", "Predicted Enrollment", "Actual Enrollment", "Pre-Reg Enrollment"]
+  column_headings = ["Semester", "Predicted Enrollment", "Prereg Predicted Enrollment", "Actual Enrollment", "Pre-Reg Enrollment"]
   semesters_dict = {semester_name: i for i, semester_name in enumerate(semester_names)}
   
   for course_no in course_list: 
@@ -18,6 +18,7 @@ def store_simulation_data(course_list, courses, semester_names, sim_data, destin
     actual_enrollment = [0] * num_semesters
     prereg_enrollment = [0] * num_semesters
     predicted_enrollment = sim_data[course_no]
+    prereg_predicted_enrollment = prereg_predicted_data[course_no]
 
     for i, desired_semester in enumerate(semester_names):
     # Sarah:
@@ -30,7 +31,7 @@ def store_simulation_data(course_list, courses, semester_names, sim_data, destin
     data.append([course_no + " - " + course_name])
     data.append(column_headings)
     for i in range(num_semesters):
-      data.append([semester_names[i], predicted_enrollment[i], actual_enrollment[i], prereg_enrollment[i]])
+      data.append([semester_names[i], predicted_enrollment[i], prereg_predicted_enrollment[i], actual_enrollment[i], prereg_enrollment[i]])
 
     ws = w.add_sheet(course_no)
     for i,row in enumerate(data):
