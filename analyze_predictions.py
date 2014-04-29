fd7e5d3c060ba43d6661fecd11c59b95735b13b5
berit <berit.johnson@students.olin.edu>
2014-04-29 12:29:13 -0400

    function to make bar graph of error

--- analyze_predictions.py
+++ analyze_predictions.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def determine_highest_weighted_courses(logistic, all_features_list, number_of_courses):
   """ Finds the classes with highest absolute value coefficients and determines whether 
       they're positive or negative
@@ -19,3 +21,26 @@ def print_highest_weighted_courses(logistic, all_features_list, number_of_course
       print course_info[1], i, correlation
     i += 1
   return temp_str
+
+def make_histograms_for_models(model_names, course_names, error_list):
+  width = .8
+  bar_starts = range(len(course_names))
+  label_center = map(lambda x: x+width/2, bar_starts)
+  num_models = len(model_names)
+  plt.figure(1)
+  for i, model_name in enumerate(model_names):
+    plt.subplot(num_models, 1, i)
+    plt.bar(bar_starts, error_list[i])
+    plt.xticks(label_center, course_names)
+    plt.ylabel('error')
+    plt.title(model_name)
+  plt.show()
+
+def test_make_histo():
+  model_names = ['model a', 'model b']
+  course_names = ['course 1', 'course 2', 'course 3']
+  error_list = [ [2, 3, 4], [5, 2, 7]]
+  make_histograms_for_models(model_names, course_names, error_list)
+
+if __name__ == '__main__':
+  test_make_histo()
